3e049038ae0a27b3b285d05546f1a09145e38f6f
rami <1034930026@qq.com>
2026-06-18 14:42:48 +0800

    Add node model

---
+++ graph-core/src/main/java/com/rungoto/rec/graph/node/NodeType.java
@@ -0,0 +1,14 @@
+package com.rungoto.rec.graph.node;
+
+public enum NodeType {
+    ACTION,
+    LOAD_FEATURE,
+    RECALL,
+    MERGE,
+    FILTER,
+    RANK,
+    RERANK,
+    EXPERIMENT,
+    SUB_GRAPH,
+    PARALLEL
+}
